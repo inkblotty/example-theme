9b8c20f6072fb45d68ef118305d992f268ae9c02
Katie Foster <fosterkatiea@gmail.com>
2021-08-19 12:34:46 -0500

    Update typography.types.ts

--- src/theme/typography/typography.types.ts
+++ src/theme/typography/typography.types.ts
@@ -2,31 +2,104 @@ export interface Typography {
   body: {
     main: {
       family: string;
-      urls: string[];
+      lineHeight: string;
+      size: {
+        sm: string; // small devices
+        lg: string; // large devices
+      };
+      weight: number;
     };
-    bold:  {
+    large: {
       family: string;
-      urls: string[];
+      lineHeight: string;
+      size: {
+        sm: string;
+        lg: string;
+      };
+      weight: number;
     };
+    small: {
+      family: string;
+      lineHeight: string;
+      size: {
+        sm: string;
+        lg: string;
+      };
+      weight: number;
+    };
+    title: { // do we want this? seems like it should be a heading
+      
+    },
   };
+  figcaption: { // this is called microtext in associated docs
+    family: string;
+    lineHeight: string;
+    size: {
+      sm: string;
+      lg: string;
+    };
+    weight: number;
+  },
   headings: {
-    main:  {
+    h1: {
       family: string;
-      urls: string[];
-    };
-    bold:  {
+      lineHeight: string;
+      size: {
+        sm: string;
+        lg: string;
+      };
+      weight: number;
+      familyUrls: string[];
+    },
+    h2: {
       family: string;
-      urls: string[];
-    };
+      lineHeight: string;
+      size: {
+        sm: string;
+        lg: string;
+      };
+      weight: number;
+      familyUrls: string[];
+    },
+    h3: {
+      family: string;
+      lineHeight: string;
+      size: {
+        sm: string;
+        lg: string;
+      };
+      weight: number;
+      familyUrls: string[];
+    },
+    h4: {
+      family: string;
+      lineHeight: string;
+      size: {
+        sm: string;
+        lg: string;
+      };
+      weight: number;
+      familyUrls: string[];
+    },
+    h5: {
+      family: string;
+      lineHeight: string;
+      size: {
+        sm: string;
+        lg: string;
+      };
+      weight: number;
+      familyUrls: string[];
+    },
+    h6: {
+      family: string;
+      lineHeight: string;
+      size: {
+        sm: string;
+        lg: string;
+      };
+      weight: number;
+      familyUrls: string[];
+    },
   };
-  alternate: {
-    main:  {
-      family: string;
-      urls: string[];
-    };
-    bold:  {
-      family: string;
-      urls: string[];
-    };
-  }
 }
